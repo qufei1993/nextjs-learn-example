5671c99ecd44cc533540a0ea1a015cfdb53f60ba
五月君 <qzfweb@gmail.com>
2024-01-27 15:27:20 +0800

    fix: set trustHost to false

--- auth.config.ts
+++ auth.config.ts
@@ -4,6 +4,7 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
+  trustHost: true,
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
